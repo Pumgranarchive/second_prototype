424be33fa3ffc095fe88a6a953ed7cabd00b4fe0
Arnaud Parant <ard.parant@gmail.com>
2014-03-27 23:51:23 +0000

    Comment couchDB part in the import.sh script

--- export_db/import.sh
+++ export_db/import.sh
@@ -2,19 +2,19 @@
 
 database=pumgrana
 
-### CouchDB
-curl -X DELETE http://127.0.0.1:5984/$database
-curl -X PUT http://127.0.0.1:5984/$database
-
-#couchdb_import database collection
-couchdb_import(){
-    curl -X PUT http://127.0.0.1:5984/$1/$2
-    curl -X PUT -d @$1/$2.cdb.json http://127.0.0.1:5984/$1/$2
-}
-
-couchdb_import $database contents
-couchdb_import $database tags
-couchdb_import $database links
+# ### CouchDB
+# curl -X DELETE http://127.0.0.1:5984/$database
+# curl -X PUT http://127.0.0.1:5984/$database
+# 
+# #couchdb_import database collection
+# couchdb_import(){
+#     curl -X PUT http://127.0.0.1:5984/$1/$2
+#     curl -X PUT -d @$1/$2.cdb.json http://127.0.0.1:5984/$1/$2
+# }
+# 
+# couchdb_import $database contents
+# ouchdb_import $database tags
+# couchdb_import $database links
 
 ### MongoDB
 mongo $database --eval "db.dropDatabase()"
